f93231a4a8cc8b978a8464adf7039411001a443c
Rachel Bazelais <bazelais.rachel@gmail.com>
2018-07-23 02:27:12 -0700

    init book list

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var book = require('./routes/book');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'})); // for debugging only
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	res.locals.message = err.message; // set locals, only providing error in development
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/mean-books', { useMongoClient: true, promiseLibrary: require('bluebird') })
+	.then(() =>  console.log('Connection to database is succesful.'))
+	.catch((err) => console.error(err));
+	
+module.exports = app;
